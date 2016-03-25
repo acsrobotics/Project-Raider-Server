316cf4e60faa18c19466a7e52615a44444325d47
GoldenTeeth <archer.xn@live.cn>
2016-03-25 13:56:59 -0400

    Updated with Intellij

---
+++ src/helper/lib/CvEngine.java
@@ -0,0 +1,379 @@
+package helper.lib;
+
+import org.opencv.core.*;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+
+/**
+ * For specific use of this library, please refer to this repository
+ * https://github.com/GoldenTeeth/azgt_wheels/tree/master/vision/CvBackend
+ * CvPipline is essentially an OpenCV wrapper library design to provide 
+ * LINQ-like API to complete simple image processing task
+ * Please make sure OpenCV_2.4.11 is properly installed on your computer
+ * and it is added to the Build Path of this project
+ * 
+ * @author Zhang
+ *
+ */
+public class CvEngine {
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+	
+	LinkedList<MatOfPoint> contours;
+	LinkedList<Rect>       rects;
+	
+	Mat Image;
+	
+	int iLowH;
+	int iLowS;
+	int iLowV;
+	
+	int iHighH;
+	int iHighS;
+	int iHighV;
+	
+	FilterPipeline filters;
+	
+	public CvEngine(){
+		this.contours = new LinkedList<>();
+		this.rects    = new LinkedList<>();
+		this.filters  = new FilterPipeline();
+		this.filters.injectPipeDependency(this);
+	}
+	
+	public CvEngine setLowHSV(int H, int S, int V){
+		this.iLowH = H;
+		this.iLowS = S;
+		this.iLowV = V;
+		return this;
+	}
+	
+	public CvEngine setHighHSV(int H, int S, int V){
+		this.iHighH = H;
+		this.iHighS = S;
+		this.iHighV = V;
+		return this;
+	}
+	
+	public CvEngine combineWith(final Mat second, double alpha){
+		
+		if(alpha > 1.0){
+			return null;
+		}
+		
+		double beta = 1.0 - alpha;
+		
+		Mat combined = new Mat(this.Image.size(), this.Image.type());
+		
+		Core.addWeighted(this.Image, alpha, second, beta, 0.0, combined);
+		this.Image = combined;
+		return this;
+	}
+	
+	public CvEngine toBGR(){
+		Mat imgBGR = new Mat(this.Image.size(), this.Image.type());
+		Imgproc.cvtColor(this.Image, imgBGR, Imgproc.COLOR_HSV2BGR);
+		this.Image = imgBGR;
+		return this;
+	}
+	
+	public CvEngine toGray(){
+		Mat imgGray = new Mat(this.Image.size(), this.Image.type());
+		Imgproc.cvtColor(this.Image, imgGray, Imgproc.COLOR_BGR2GRAY);
+		this.Image = imgGray;
+		return this;
+		
+	}
+	
+	public CvEngine fitEllipse(){
+		
+		
+		return this;
+	}
+	
+	public CvEngine findContours(){
+		Imgproc.findContours(this.Image, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+		return this;
+	}
+	
+	public CvEngine computeRectsFromContours(){
+		MatOfPoint2f approxCurve = new MatOfPoint2f();
+		for(int i=0; i<contours.size(); i++){
+			// Convert contours(i) from MatOfPoint to MatOfPoint2f
+			MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+			// processing on mMOP2f1 which is in type MatOfPoint2f
+			double approxDistance = Imgproc.arcLength(contour2f, true) * 0.02;
+			Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+			
+			// convert back to MatOfPoints
+			MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+			Rect rect = Imgproc.boundingRect(points);
+			
+			if(filters.eval(rect)){
+				this.rects.add(rect);
+			}
+			
+		}
+		return this;
+	}
+	
+	public CvEngine drawCircleOnCenter(){
+		if(this.rects.size() != 0){
+			double x_center = this.Image.size().width / 2;
+			double y_center = this.Image.size().height / 2;
+			
+			// set the radius of the circle to 70% of the rectangle's width 
+			double radius = this.rects.getFirst().width / 2 * 0.75;
+			
+			Scalar color = onTarget(x_center, y_center, radius) ? 
+							new Scalar(0, 255, 0) : new Scalar(0, 0, 255);
+							
+			Core.circle(this.Image, new Point(x_center, y_center), (int) radius, color, 2);
+		}
+		
+		return this;
+	}
+	
+	public CvEngine reduceRectsToOne(){
+		int highestPosition = 0;
+		for(int i=0; i<this.rects.size(); i++){
+			highestPosition = this.rects.get(i).y > highestPosition ? this.rects.get(i).y : highestPosition;
+		}
+		for(int i=0; i<this.rects.size(); i++){
+			if(this.rects.get(i).y < highestPosition){
+				this.rects.remove(i);
+			}
+		}
+		if(this.rects.size() > 1){
+			Rect result = this.rects.pop();
+			this.rects.clear();
+			this.rects.add(result);
+		}
+		return this;
+	}
+	
+	public List<MatOfPoint> getContours(){
+		return this.contours;
+	}
+	
+	public List<Rect> getRects(){
+		return this.rects;
+	}
+	
+	public CvEngine drawRects(Mat drawing){
+		if(drawing == null){
+			drawing = Mat.zeros(this.Image.size(), CvType.CV_8UC3);
+		}
+		
+		for(int i=0; i<rects.size(); i++){
+			Rect rect = this.rects.get(i);
+			Core.rectangle(drawing, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0,255,0), 2);
+		}
+		
+		this.Image = drawing;
+		return this;
+	}
+	
+	public CvEngine resizeTo(int width, int height){
+		Mat out = new Mat();
+		Imgproc.resize(this.Image, out, new Size(width, height));
+		this.Image = out;
+		return this;
+	}
+	
+	public CvEngine detectFaces(CascadeClassifier classifier){
+		Mat frame = new Mat();
+		Imgproc.equalizeHist(this.Image, frame);
+		MatOfRect rects = new MatOfRect();
+		classifier.detectMultiScale(frame, rects, 2.2, 2, 0, new Size(60,60), new Size(300, 300));
+		for(Rect r : rects.toArray()){
+			if(filters.eval(r)){
+				this.rects.add(r);
+			}
+		}
+		return this;
+	}
+	
+	public CvEngine momentTrack(final Mat imgOriginal, final Mat imgThresholded){
+		Mat imgProcessed = imgOriginal.clone();
+		Size size = new Size(imgOriginal.width(), imgOriginal.height());
+		Moments oMoments = Imgproc.moments(imgThresholded);
+		
+		double dM01 = oMoments.get_m01();
+		double dM10 = oMoments.get_m10();
+		double dArea = oMoments.get_m00();
+		
+		if(dArea > 10000){
+			int posX = (int) (dM10 / dArea);
+			int posY = (int) (dM01 / dArea);
+			
+			if(posX >=0 && posY >=0){
+				Point target = new Point(posX, posY);
+				Point center = new Point(size.width/2, size.height/2);
+				
+				Core.circle(imgProcessed, target, 15, new Scalar(0,255,0), 2);
+				if(onTarget(center, target, 60)){
+					Core.circle(imgProcessed, center, 60, new Scalar(0,255), 2);
+				}
+
+			}
+		}
+		this.Image = imgProcessed;
+		return this;
+	}
+	
+	public CvEngine addFilter(FilterPipeline.Filter filter){
+		this.filters.addFilter(filter);
+		return this;
+	}
+	
+	public int[] computeRectRelativeDifference(){
+		if(this.rects.size() != 1){
+			int[] ret = {-1,-1};
+			return ret;
+		}
+		int[] center = {(int) (this.Image.size().width / 2), (int) (this.Image.size().height / 2)};
+		int[] rect = {this.rects.getFirst().x, this.rects.getFirst().y};
+		int[] difference = {rect[0] - center[0], center[1] - rect[1]};
+		return difference;
+	}
+	
+	
+	/**
+	 * calculate whether the circle is on target(rectangle) or not
+	 * @param x_center
+	 * @param y_center
+	 * @param radius
+	 * @return
+	 */
+	private boolean onTarget(double x_center, double y_center, double radius){
+		// calculate the order in x_left, x_right, y_top, y_bottom
+		Rect rect = this.rects.getFirst();
+		double[] rectangle = {
+				rect.x,
+				rect.x + (rect.width),
+				rect.y,
+				rect.y + (rect.height )
+		};
+		
+		double[] circle = {
+				x_center - radius,
+				x_center + radius,
+				y_center - radius,
+				y_center + radius
+		};
+		
+		boolean status;
+		
+		
+		// if circle is within the rectangle 
+		if(rectangle[0] < circle[0] &&
+			rectangle[1] > circle[1] &&
+			rectangle[2] < circle[2] &&
+			rectangle[3] > circle[3]){
+			status = true;
+		}else{
+			status = false;
+		}
+		
+		return status;
+	}
+	
+	public CvEngine convertToThreeChannel(){
+		if(this.Image.type() != CvType.CV_8UC3){
+			Mat out = new Mat(this.Image.size(), CvType.CV_8UC3);
+			List<Mat> in = new ArrayList<>(3);
+			for(int i=0; i<3; i++){
+				in.add(this.Image);
+			}
+			Core.merge(in, out);
+			this.Image = out;
+		}
+		return this;
+	}
+	
+	public CvEngine toHSV(){
+		Mat imgHSV = new Mat(this.Image.size(), this.Image.type());
+		Imgproc.cvtColor(this.Image, imgHSV, Imgproc.COLOR_BGR2HSV);
+		this.Image = imgHSV;
+		return this;
+	}
+	
+	public CvEngine threshold(){
+		Mat imgProcessed = new Mat(this.Image.size(), this.Image.type());
+		this.Image.copyTo(imgProcessed);
+		Size size = imgProcessed.size();
+		Core.circle(imgProcessed, new Point(size.width/2, size.height/2), 60, new Scalar(0,0,255), 2);
+		
+		Mat imgThresholded = new Mat(size, imgProcessed.type());
+		Core.inRange(this.Image, new Scalar(
+								this.iLowH
+								,this.iLowS
+								,this.iLowV), 
+							new Scalar(
+									this.iHighH
+									,this.iHighS
+									,this.iHighV), imgThresholded);
+		
+		preProcess(imgThresholded);
+		this.Image = imgThresholded;
+		return this;
+	}
+	
+	public CvEngine invert(){
+		Mat inverted = new Mat(this.Image.size(), this.Image.type());
+		Imgproc.threshold(this.Image, inverted, 1, 255, Imgproc.THRESH_BINARY_INV);
+		this.Image = inverted;
+		return this;
+	}
+	
+	private boolean onTarget(Point center, Point target, int radius){
+		int x_diff = (int) Math.abs(center.x - target.x);
+		int y_diff = (int) Math.abs(center.y - target.y);
+		int distance = (int) Math.sqrt(x_diff * x_diff + y_diff * y_diff);
+		if(distance < radius){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	private void preProcess(Mat img){
+		Imgproc.erode(img, img, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5,5)));
+		Imgproc.dilate(img, img, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5,5)));
+		Imgproc.dilate(img, img, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5,5)));
+		Imgproc.erode(img, img, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5,5)));
+	}
+	
+	
+	public CvEngine writeToFileWithName(String fileName){
+		Highgui.imwrite(fileName + ".jpg", this.Image);
+		return this;
+	}
+	
+	public CvEngine getBlackEmptyMat(Mat sample){
+		this.Image = new Mat(sample.size(), sample.type(), new Scalar(0,0,0));
+		return this;
+	}
+	
+	public CvEngine setImage(Mat img){
+		this.Image = img;
+		return this;
+	}
+	
+	public Mat getImage(){
+		return this.Image;
+	}
+	
+}
---
+++ src/helper/module/VideoCap.java
@@ -0,0 +1,162 @@
+package helper.module;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.net.NoRouteToHostException;
+import java.util.ArrayList;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+
+/**
+ * All status code update should be handled at this layer
+ * @author Zhang
+ *
+ */
+public class VideoCap {
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+	
+
+	public enum Status{
+		AOK(0),
+		INVALID_CAMERA(1),
+		READ_ERROR(2);
+		
+		private final int code;
+		
+		Status(int code){
+			this.code = code;
+		}
+		
+		public int getCode(){
+			return this.code;
+		}
+		
+	}
+	
+	
+	public static final int RAW       = 0;
+	public static final int THRESHED  = 1;
+	public static final int PROCESSED = 2;
+	
+	VideoCapture cap;
+	ImageModule imgModule;
+	
+	LoggerModule proccedImgLogger;
+	Thread proccedImgLoggerThread;
+	
+	LoggerModule thresholdedImgLogger;
+	Thread thresholdedImgLoggerThread;
+	
+	Status status;
+	
+	String hostName = "C:\\Users\\Zhang\\Downloads\\temp\\XX.mp4";
+	// Camera: "http://axis-camera.local/mjpg/video.mjpg"
+	// Field footage: "C:\\Users\\Zhang\\Documents\\Share\\dior.mp4"
+	// Just for fun: "C:\\Users\\Zhang\\Downloads\\temp\\XX.mp4"
+	
+	public VideoCap(ImageModule imgModule) {
+		
+		cap = new VideoCapture();
+
+		cap.open(this.hostName);
+			
+		this.imgModule = imgModule;
+		
+		this.proccedImgLogger = new LoggerModule("Processed.mp4");
+		this.thresholdedImgLogger = new LoggerModule("Thresholded.mp4");
+		
+		this.proccedImgLoggerThread = new Thread(this.proccedImgLogger);
+		this.thresholdedImgLoggerThread = new Thread(this.thresholdedImgLogger);
+		
+		this.thresholdedImgLoggerThread.start();
+		this.proccedImgLoggerThread.start();
+	}
+	
+	public BufferedImage[] getOneFrame() throws IOException{
+		Mat currentFrame = new Mat();
+		ArrayList<BufferedImage> results = new ArrayList<>(3);
+		
+		
+
+		
+		try{		
+			if (!cap.read(currentFrame)) {
+				this.setStatus(Status.READ_ERROR);
+				throw new NoRouteToHostException("Fetching new Frame failed");
+			}
+			imgModule.setImgOriginal(currentFrame);
+			this.setStatus(Status.AOK);
+		}catch(Exception e){
+			throw e;
+		}
+		
+		imgModule.processCurrentFrame();
+		
+		results.add(toBufferedImage(imgModule.getImgOriginal()));
+		results.add(toBufferedImage(imgModule.getImgThresholded()));
+		results.add(toBufferedImage(imgModule.getImgProcessed()));
+		
+		// load image to background thread to write to a file
+		this.proccedImgLogger.pendingOutputImage(results.get(PROCESSED));
+		this.thresholdedImgLogger.pendingOutputImage(results.get(THRESHED));
+		
+		return results.toArray(new BufferedImage[3]);
+	}
+	
+	public void tryConnectCamera() throws NoRouteToHostException{
+		
+		// if it is already opened 
+		if(cap.isOpened()){
+			// close it
+			cap.release();
+		}
+		
+		// try open
+		cap.open(this.hostName);
+		
+		// if it is not opened, keep trying
+		if(!cap.isOpened()){
+			this.setStatus(Status.INVALID_CAMERA);
+			System.err.println("Retry");
+			throw new NoRouteToHostException();
+		}
+		
+		this.setStatus(Status.AOK);
+	}
+	
+	public BufferedImage toBufferedImage(Mat mat) throws IOException {
+		int type = 0;
+		if (mat.channels() == 1) {
+			type = BufferedImage.TYPE_BYTE_GRAY;
+		} else if (mat.channels() == 3) {
+			type = BufferedImage.TYPE_3BYTE_BGR;
+		}
+		BufferedImage img = new BufferedImage(640, 480, type);
+		WritableRaster raster = img.getRaster();
+		DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+		byte[] data = dataBuffer.getData();
+		mat.get(0, 0, data);
+		return img;
+	}
+	
+	public void endRecording(){
+		this.proccedImgLogger.RUNNING_FLAG = false;
+		this.thresholdedImgLogger.RUNNING_FLAG = false;
+	}
+
+	public synchronized void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public synchronized Status getStatus(){
+		return status;
+	}
+
+}
